bef54a737b255e841a14de9a4f0530d2e7337874
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2017-11-28 21:41:42 +0200

    feat - inital commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method}, ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         page_title: 'Website is in maintenance'
+//     })
+// });
+
+app.use(express.static(__dirname + '/Public'));
+
+hbs.registerHelper('get_current_year', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('scream_it', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        page_title: 'Home page',
+        welcome_message: 'Welcome to my website',
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        page_title: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'Unable to make request'
+    })
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Express server started, listening on port ${PORT}`)
+});
